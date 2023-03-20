d317f9f29cf662e8d001dd8a8fc31165f60f3843
AlyaKagerou <alyazalfa46@gmail.com>
2023-03-20 21:21:00 +0700

    meminimalisir code di tag script

---
+++ main.js
@@ -0,0 +1,66 @@
+var swiper = new Swiper(".hololive-news", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: "swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
+var swiper = new Swiper(".talent", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: "swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+    },
+});
+var swiper = new Swiper(".mv", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: "swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+});
